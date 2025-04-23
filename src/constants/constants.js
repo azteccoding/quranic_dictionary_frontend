369ae82526773bfc3862eeaf39da07402f6cfc45
aztecdev <contactos@mnaztecdev.com>
2025-04-22 23:18:03 -0600

    added local lambda testing

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,3 +2,8 @@ export const ES2AR_API_URI =
   "https://quranicarabicspanishdictionary.netlify.app/.netlify/functions/spanish?word=";
 export const AR2ES_API_URI =
   "https://quranicarabicspanishdictionary.netlify.app/.netlify/functions/arabic?word=";
+
+export const ES2AR_API_URI_LOCAL =
+  "http://localhost:8888/.netlify/functions/spanish?word=";
+export const AR2ES_API_URI_LOCAL =
+  "http://localhost:8888/.netlify/functions/arabic?word=";
