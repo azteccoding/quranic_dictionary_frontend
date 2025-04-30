519cd63215c01fe91ffaa3e3007af0c2e674d257
aztecdev <contactos@mnaztecdev.com>
2025-04-29 21:50:59 -0600

    fix synonym

--- src/components/SearchSynonyms.jsx
+++ src/components/SearchSynonyms.jsx
@@ -1,7 +1,7 @@
 const SearchSynonyms = (props) => {
   return (
     <div>
-      <div className="synonym arabic-word-xs">Sinónimos</div>
+      <div className="arabic-word-xs">Sinónimos</div>
       {props.synonyms.map((syn) => (
         <p
           className="synonym arabic-word-s"
