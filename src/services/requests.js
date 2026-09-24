a161911e08019d3843fafb1f083ae3e67403b505
aztecdev <contactos@mnaztecdev.com>
2026-09-23 18:42:23 -0600

    create word screen

--- src/services/requests.js
+++ src/services/requests.js
@@ -3,15 +3,12 @@ import {
   AR2ES_API_URI,
   ES2AR_API_URI,
   CREATE_WORD_API_URI,
-  CREATE_WORD_API_URI_LOCAL,
-  AR2ES_API_URI_LOCAL,
-  ES2AR_API_URI_LOCAL,
 } from "../constants/constants";
 
 // Make a request for a user with a given ID
 export const findInDictionary = async (word, arab2EspSelected) => {
   const URL_with_query_params =
-    (arab2EspSelected ? AR2ES_API_URI_LOCAL : ES2AR_API_URI_LOCAL) + word;
+    (arab2EspSelected ? AR2ES_API_URI : ES2AR_API_URI) + word;
   try {
     return {
       hasExternalError: false,
@@ -32,7 +29,7 @@ export const createWord = async (wordData) => {
   try {
     return {
       hasExternalError: false,
-      data: await axios.post(CREATE_WORD_API_URI_LOCAL, wordData, {
+      data: await axios.post(CREATE_WORD_API_URI, wordData, {
         headers: {
           "Content-Type": "application/json",
           "x-api-key": "admin+chingon",
